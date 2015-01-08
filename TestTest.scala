79d95944ef4a25a04405a0f0b7a814d69009c6b0
Jaap Taal <jaap@q42.nl>
2015-01-08 19:08:24 +0100

    Added a case where it works and where it doesn't work too

--- TestTest.scala
+++ TestTest.scala
@@ -2,8 +2,23 @@ trait T
 case class X() extends T
 
 object TestTest {
-  implicit val xm = new OMaterializer[X] {} // The OMaterializer trait here is the second part of the problem
+  // The OMaterializer trait here is the second part of the problem
+  implicit val xm = new OMaterializer[X] {}
 
   // the explicit `tm: Materializer[T]` type declaration here is first part of the problem
   implicit val tm: Materializer[T] = Macro.genMaterializer[T, X]
+}
+
+//object ThisDoesntWorkToo {
+//  implicit val xm = new OMaterializer[X] {}
+//
+//  implicit val tm: Materializer[T] = withoutMacro[T, X]
+//
+//  def withoutMacro[A, B](implicit m: Materializer[B]): Materializer[A] = new Materializer[A] {}
+//}
+
+object ThisWorks {
+  implicit val xm: Materializer[X] = new OMaterializer[X] {}
+  implicit val tm: Materializer[T] = withoutMacro[T, X]
+  def withoutMacro[A, B](implicit m: Materializer[B]): Materializer[A] = new Materializer[A] {}
 }
